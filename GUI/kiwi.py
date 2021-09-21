ce1260d962ba8a4afbf0eac2cb200a9d01d9231e
Yugam4254 <yugamsehgal4254@gmail.com>
2021-09-21 23:45:31 +0530

    added icon

--- GUI/kiwi.py
+++ GUI/kiwi.py
@@ -4,7 +4,13 @@ from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
+from kivy.clock import Clock
+import random
+from kivy.uix.gridlayout import GridLayout
+from kivy.properties import StringProperty
+from kivy.core.window import Window
 
+Window.size=(600,500)
 kv = Builder.load_file("mymain.kv")
 
 class Home(Screen):
@@ -12,7 +18,12 @@ class Home(Screen):
 class GameMode(Screen):
     pass
 class Quiz(Screen):
-    pass
+    o=1
+    global ok
+    ok=f"nice th{o}s worked"
+
+    textin=StringProperty(f"{ok}")
+
 class RapidFire(Screen):
     pass
 class Facts(Screen):
@@ -27,6 +38,8 @@ class WindowManager(ScreenManager):
     pass
 
 class MyMainApp(App):
+    title="Space"
+    icon="black-hole-icon.ico"
     def build(self):
         return kv
 
