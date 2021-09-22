f779028fb18ea8fe2a8408123e838f71dc38a017
Yugam4254 <yugamsehgal4254@gmail.com>
2021-09-22 19:48:31 +0530

    quiz design (text aligned)

--- GUI/kiwi.py
+++ GUI/kiwi.py
@@ -3,16 +3,16 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.gridlayout import GridLayout
-from kivy.properties import ObjectProperty
-from kivy.properties import StringProperty
-from kivy.lang import Builder
-from kivy.clock import Clock
-from kivy.core.window import Window
-
+from kivy.uix.label import Label
 from kivy.uix.checkbox import CheckBox
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.lang import Builder
+from kivy.clock import Clock
+from kivy.core.window import Window
+from kivy.properties import ObjectProperty
+from kivy.properties import StringProperty
 
 Window.size=(600,500)
 kv = Builder.load_file("mymain.kv")
@@ -46,10 +46,10 @@ class Quiz(Screen):
     r=random.randint(1,len(ques)-1)
     ans1,ans2,ans3,ans4=ques[r][2],ques[r][3],ques[r][4],ques[r][5]
     question=StringProperty(f"\nQuestion {str(ques_no)}: {ques[r][1]}")
-    opt1=StringProperty(f"1. {ques[r][2]}")
-    opt2=StringProperty(f"2. {ques[r][3]}")
-    opt3=StringProperty(f"3. {ques[r][4]}")
-    opt4=StringProperty(f"4. {ques[r][5]}")
+    opt1=StringProperty(f"1.    {ques[r][2]}")
+    opt2=StringProperty(f"2.    {ques[r][3]}")
+    opt3=StringProperty(f"3.    {ques[r][4]}")
+    opt4=StringProperty(f"4.    {ques[r][5]}")
      
     # removing the used ques for this quiz
     looplist.insert(0, ques.pop(r))
@@ -86,6 +86,15 @@ class EndScreen(Screen):
     pass
 class WindowManager(ScreenManager):
     pass
+
+#misc requirements
+class WrappedLabel(Label):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.bind(
+            width=lambda *x:
+            self.setter('text_size')(self, (self.width, None)),
+            texture_size=lambda *x: self.setter('height')(self, self.texture_size[1]))
 
 class MyMainApp(App):
     title="Space"
