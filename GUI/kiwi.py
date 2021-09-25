bade575b80e3c542ca7c80dc02c7f9da670ca5d3
Yugam4254 <yugamsehgal4254@gmail.com>
2021-09-25 09:38:55 +0530

    quizques2,3,4,5 reduced to atoms

--- GUI/kiwi.py
+++ GUI/kiwi.py
@@ -1,4 +1,3 @@
-from re import A
 import kivy
 from kivy.metrics import dp
 from kivy.app import App
@@ -13,6 +12,7 @@ from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.lang import Builder
 from kivy.clock import Clock
 from kivy.core.window import Window
+from kivy.uix.scrollview import ScrollView
 from kivy.properties import ObjectProperty
 from kivy.properties import StringProperty
 
@@ -33,7 +33,7 @@ read_ques_file=csv.reader(ques_file)
 ques=[]         # 1-Q, 2-O1, 3-O2, 4-O3, 5-O4, 6-A
 for x in read_ques_file:
     ques.append(x)
-
+r=1
 looplist=[]
 class Home(Screen):
     pass
@@ -41,40 +41,66 @@ class Home(Screen):
 class Setting(Screen):
     pass
 
+class Info(Screen):
+    file=open("GUI/info.txt")
+    info=file.read()
+    infos = StringProperty(f"{info}")
+
 class GameMode(Screen):
     pass
-r=random.randint(1,len(ques)-1)
-quiz_score = 0
-check=""
 
-class QuizQues1(Screen):
-    global quiz_score
+class Quiz(Screen):
+    quiz_score = StringProperty("0")
     global ques
     global looplist
-    global r
-    global check
+    check=StringProperty("")
     
-    A=0
-    B=0
-    C=0
-    D=0
+    A=StringProperty("0")
+    B=StringProperty("0")
+    C=StringProperty("0")
+    D=StringProperty("0")
     ans=StringProperty(f"{check}")
+    nexttohome=StringProperty("Next")
 
-    #functions
+    ques_no=1
+    global r
     r=random.randint(1,len(ques)-1)
     ans1,ans2,ans3,ans4=ques[r][2],ques[r][3],ques[r][4],ques[r][5]
-    question=StringProperty(f"\nQuestion 1: {ques[r][1]}")
+    question=StringProperty(f"\nQuestion {int(ques_no)}: {ques[r][1]}")
     opt1=StringProperty(f"1.    {ques[r][2]}")
     opt2=StringProperty(f"2.    {ques[r][3]}")
     opt3=StringProperty(f"3.    {ques[r][4]}")
     opt4=StringProperty(f"4.    {ques[r][5]}")
-    
+
     # removing the used ques for this quiz
     looplist.insert(0, ques.pop(r))
 
     if len(ques)==1:
         for i in range(len(looplist)):
             ques.insert(1, looplist.pop(i))
+
+    def start_quiz_ques(self):
+        if self.ques_no==4:
+            self.nexttohome="Home"
+        
+        #functions
+        self.ques_no+=1
+        r=random.randint(1,len(ques)-1)
+        self.ans1,self.ans2,self.ans3,self.ans4=ques[r][2],ques[r][3],ques[r][4],ques[r][5]
+        self.question=f"\nQuestion {int(self.ques_no)}: {ques[r][1]}"
+        self.opt1=f"1.    {ques[r][2]}"
+        self.opt2=f"2.    {ques[r][3]}"
+        self.opt3=f"3.    {ques[r][4]}"
+        self.opt4=f"4.    {ques[r][5]}"
+        
+        # removing the used ques for this quiz
+        looplist.insert(0, ques.pop(r))
+
+        if len(ques)==1:
+            for i in range(len(looplist)):
+                ques.insert(1, looplist.pop(i))
+
+        
 
     def inputA(self, button):
         self.A="1"
@@ -89,102 +115,12 @@ class QuizQues1(Screen):
         global check
         if (self.A=="1" and self.ans1==ques[r][6]) or (self.A=="2" and self.ans2==ques[r][6])\
         or (self.A=="3" and self.ans3==ques[r][6]) or (self.A=="4" and self.ans4==ques[r][6]):
-            quiz_score += 1
+            self.quiz_score += 1
             check="true"
         else:
             check="false"
         
-        self.ans=StringProperty(f"{check}")
-
-class QuizQues2(Screen):
-    global quiz_score
-    global ques
-    global looplist
-    global r
-
-    #functions
-    r=random.randint(1,len(ques)-1)
-    ans1,ans2,ans3,ans4=ques[r][2],ques[r][3],ques[r][4],ques[r][5]
-    question=StringProperty(f"\nQuestion 2: {ques[r][1]}")
-    opt1=StringProperty(f"1.    {ques[r][2]}")
-    opt2=StringProperty(f"2.    {ques[r][3]}")
-    opt3=StringProperty(f"3.    {ques[r][4]}")
-    opt4=StringProperty(f"4.    {ques[r][5]}")
-     
-    # removing the used ques for this quiz
-    looplist.insert(0, ques.pop(r))
-
-    if len(ques)==1:
-        for i in range(len(looplist)):
-            ques.insert(1, looplist.pop(i))
-
-class QuizQues3(Screen):
-    global quiz_score
-    global ques
-    global looplist
-    global r
-    
-    #functions
-    r=random.randint(1,len(ques)-1)
-    ans1,ans2,ans3,ans4=ques[r][2],ques[r][3],ques[r][4],ques[r][5]
-    question=StringProperty(f"\nQuestion 3: {ques[r][1]}")
-    opt1=StringProperty(f"1.    {ques[r][2]}")
-    opt2=StringProperty(f"2.    {ques[r][3]}")
-    opt3=StringProperty(f"3.    {ques[r][4]}")
-    opt4=StringProperty(f"4.    {ques[r][5]}")
-     
-    # removing the used ques for this quiz
-    looplist.insert(0, ques.pop(r))
-
-    if len(ques)==1:
-        for i in range(len(looplist)):
-            ques.insert(1, looplist.pop(i))
-
-class QuizQues4(Screen):
-    global quiz_score
-    global ques
-    global looplist
-    global r
-    
-    #functions
-    r=random.randint(1,len(ques)-1)
-    ans1,ans2,ans3,ans4=ques[r][2],ques[r][3],ques[r][4],ques[r][5]
-    question=StringProperty(f"\nQuestion 4: {ques[r][1]}")
-    opt1=StringProperty(f"1.    {ques[r][2]}")
-    opt2=StringProperty(f"2.    {ques[r][3]}")
-    opt3=StringProperty(f"3.    {ques[r][4]}")
-    opt4=StringProperty(f"4.    {ques[r][5]}")
-     
-    # removing the used ques for this quiz
-    looplist.insert(0, ques.pop(r))
-
-    if len(ques)==1:
-        for i in range(len(looplist)):
-            ques.insert(1, looplist.pop(i))
-
-class QuizQues5(Screen):
-    global quiz_score
-    global ques
-    global looplist
-    global r
-
-    #functions
-    r=random.randint(1,len(ques)-1)
-    ans1,ans2,ans3,ans4=ques[r][2],ques[r][3],ques[r][4],ques[r][5]
-    question=StringProperty(f"\nQuestion 5: {ques[r][1]}")
-    opt1=StringProperty(f"1.    {ques[r][2]}")
-    opt2=StringProperty(f"2.    {ques[r][3]}")
-    opt3=StringProperty(f"3.    {ques[r][4]}")
-    opt4=StringProperty(f"4.    {ques[r][5]}")
-     
-    # removing the used ques for this quiz
-    looplist.insert(0, ques.pop(r))
-
-    if len(ques)==1:
-        for i in range(len(looplist)):
-            ques.insert(1, looplist.pop(i))
-
-    quiz_score=0
+        self.ans=f"{check}"
 
 class RapidFire(Screen):
     pass
