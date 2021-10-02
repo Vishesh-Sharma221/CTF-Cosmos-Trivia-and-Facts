9ed930ec410cf6fb62682afd3121f7772c85f71b
Yugam4254 <yugamsehgal4254@gmail.com>
2021-10-02 12:55:47 +0530

    testv8

--- Test/main.py
+++ Test/main.py
@@ -16,6 +16,8 @@ class canvaswidget(Widget):
             Color(.52,.52,.52)
             self.rect=Rectangle(pos=self.pos, source="images/infobutton.png")
 
+    
+
 class MyApp(App):
     def build(self):
         b=BoxLayout(orientation="vertical")
@@ -26,15 +28,18 @@ class MyApp(App):
                 font_size=150,
                 pos=(200,10),
                 font_name="fonts/righteous.ttf")
-        
+        file=open("Test/info.txt")
+        lines=file.readlines()
+    
+        l2=Label(text=lines[3])
         f=FloatLayout()
         s=Scatter(pos= (200,200))
 
-        t.bind(text=l.setter("text"))
+        t.bind(text=l2.setter("text"))
 
         f.add_widget(s)
         s.add_widget(canvaswidget())
-        s.add_widget(l)
+        s.add_widget(l2)
 
         b.add_widget(t)
         b.add_widget(f)
