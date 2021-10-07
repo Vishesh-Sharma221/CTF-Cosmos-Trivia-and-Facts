b33c982e13e767fbb254a73a760fbbde92585dc7
vishesh <sharmavishesh221@gmail.com>
2021-10-07 11:42:05 +0530

    test commit

--- CTF/main.py
+++ CTF/main.py
@@ -509,7 +509,7 @@ class CTFApp(App):
         return kvfile
     
     #tts func to make our program say something
-    engine = pyttsx3.init('sapi5')
+    engine = pyttsx3.init()
     voices = engine.getProperty('voices')
     engine.setProperty("voices", voices[0].id)
     engine.setProperty("rate", 178)
